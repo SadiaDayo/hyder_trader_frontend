3d58c29c5fe30c6a69bcc6fe9131ed70bf192c9d
Sadia Dayo <144684446+SadiaDayo@users.noreply.github.com>
2026-03-05 21:09:09 +0500

    Update WhatsAppFloat component

--- src/components/WhatsAppFloat.js
+++ src/components/WhatsAppFloat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import "../styles/WhatsAppFloat.css";
 
 const WhatsAppFloat = () => {
-  const phone = "923352043678"; // ✅ put client number (no +)
+  const phone = "923001239856"; // ✅ put client number (no +)
   const message = encodeURIComponent(
     "Assalamualaikum! I need help regarding products/prices. Please guide me."
   );
